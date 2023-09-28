2bff666b845a58655ff05e6759bb2d61a84be3b7
fearlessserver <senatoreniola@gmail.com>
2023-09-28 14:43:03 +0100

    day_2 = 28 sep

---
+++ day_2/variables.py
@@ -0,0 +1,22 @@
+# Day 2: 30 Days of Python programming
+first_name = "Senator"
+last_name = "Olorunnisola"
+full_name = "Senator Olorunnisola Eniola"
+country = "Australia"
+city = "broken hills"
+age = "18"
+year = "2005"
+is_married = "No"
+is_true = "True"
+is_light = "True"
+
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light))
