bd605ba05c33729308abeec87459195c38851f20
senator olorunnisola <senatoreniola@gmail.com>
2024-04-06 03:18:48 +0100

    len function

--- day_4/text.py
+++ day_4/text.py
@@ -1,5 +1,12 @@
 # print("bye")
-print("Hello World!")
+# print("Hello World!")
 # input would get user input from console
 # then print() will print the world "hello" and the user input
-print("hello " + input("what is your name?"))
+# print("hello " + input("what is your name?"))
+
+
+# print(input())
+# print(input())
+
+numofcharacters = len(input())
+print(numofcharacters)
