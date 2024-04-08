f086a5f156224fe4aece63f71704670ed763a35d
senator olorunnisola <senatoreniola@gmail.com>
2024-04-08 05:06:31 +0100

    updated the rollercoaster code

--- day_4/day3.py
+++ day_4/day3.py
@@ -4,13 +4,20 @@
 # else:
 # do this
 
-# print("Welcome to the rollercoaster")
-# height = int(input("What is your height in cm? "))
+print("Welcome to the rollercoaster")
+height = int(input("What is your height in cm? "))
 
-# if height >= 120:
-#     print("You can ride the rollercoaster")
-# else:
-#     print("Sorry, you have to grow taller before you can ride the rollercoaster")
+if height >= 120:
+    print("You can ride the rollercoaster")
+    age = int(input("what is your age? "))
+    if age < 12:
+        print("Please pay $5.00")
+    elif age <= 18:
+        print("Please pay $7.00")
+    else:
+        print("Please pay $12")
+else:
+    print("Sorry, you have to grow taller before you can ride the rollercoaster")
 
 
 # comparison operators
@@ -24,3 +31,14 @@
 
 # The modulo is written as a percentage sign (%) in Python.
 # It gives you the remainder after a division
+
+
+# nesting if statement
+
+# if condition:
+#   if another condition:
+#       do this
+#   else:
+#       do this
+# else:
+#   do this
