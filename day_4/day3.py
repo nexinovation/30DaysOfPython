141f9de49692e4718df1e716102f5a896d4efc7d
senator olorunnisola <senatoreniola@gmail.com>
2024-04-08 04:33:21 +0100

    comparison operators

--- day_4/day3.py
+++ day_4/day3.py
@@ -4,10 +4,19 @@
 # else:
 # do this
 
-print("Welcome to the rollercoaster")
-height = int(input("What is your height in cm? "))
+# print("Welcome to the rollercoaster")
+# height = int(input("What is your height in cm? "))
 
-if height >= 120:
-    print("You can ride the rollercoaster")
-else:
-    print("Sorry, you have to grow taller before you can ride the rollercoaster")
+# if height >= 120:
+#     print("You can ride the rollercoaster")
+# else:
+#     print("Sorry, you have to grow taller before you can ride the rollercoaster")
+
+
+# comparison operators
+# >   greater than
+# <   less than
+# >=  greater than or equal to
+# <=  less than or equal to
+# ==    equal to
+# !=    not equal to
