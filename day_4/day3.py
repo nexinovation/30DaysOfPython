90ba6b28d8c291e9e0a038b869ecfb06a7f2c821
senator olorunnisola <senatoreniola@gmail.com>
2024-04-11 07:36:04 +0100

    a pizza delivery prize calculator that takes the order of the user and calculates the total price

--- day_4/day3.py
+++ day_4/day3.py
@@ -46,26 +46,26 @@
 # quiz bmi 2.0
 
 # quiz 2 leap year calculator
-year = int(input())
+# year = int(input())
 
-FirstCondition = year % 4
-SecondCondition = year % 100
-ThridCondition = year % 400
+# FirstCondition = year % 4
+# SecondCondition = year % 100
+# ThridCondition = year % 400
 
-if FirstCondition > 0:
-    print("Not leap year")
-elif SecondCondition != 0:
-    print("leap year")
-elif ThridCondition > 0:
-    print("Not leap year")
-else:
-    print("leap year")
+# if FirstCondition > 0:
+#     print("Not leap year")
+# elif SecondCondition != 0:
+#     print("leap year")
+# elif ThridCondition > 0:
+#     print("Not leap year")
+# else:
+#     print("leap year")
 
 
 # multipe if statement
 #    if / elif /else
 #   if condition1:
-    #   do A
+#   do A
 #   elif condition2:
 #       do B
 #   else:
@@ -78,3 +78,30 @@ else:
 #  do B
 # if condition3:
 #  do C
+
+
+# another code challenge pizza delivery
+print("Thank you for choosing Python Pizza Deliveries!")
+size = input()  # What size pizza do you want? S, M, or L
+add_pepperoni = input()  # Do you want pepperoni? Y or N
+extra_cheese = input()  # Do you want extra cheese? Y or N
+
+bill = 0
+if size == "S":
+    bill = 15
+if size == "M":
+    bill = 20
+elif size == "L":
+    bill = 25
+
+# print(bill)
+if add_pepperoni == "Y":
+    if bill == 15:
+        bill += 2
+    elif bill == 20:
+        bill += 3
+    elif bill == 25:
+        bill += 3
+if extra_cheese == "Y":
+    bill += 1
+print(f"Your final bill is: ${bill}.")
